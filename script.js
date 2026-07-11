d437481498714f4d6680ef9c2264097b77028a17
shaileshsharma8oct@gmail.com <shaileshsharma8oct@gmail.com>
2026-07-11 16:08:07 +0530

    Initial commit with secure config

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// 1. Firebase Initialize
+firebase.initializeApp(CONFIG.firebase);
+const database = firebase.database();
+
+// 2. ImgBB API Key Variable (Jahan bhi zarurat ho wahan use kar lena)
+const imgbbKey = CONFIG.imgbb.apiKey;
+
+// 3. Baaki tumhara sara logic
+window.onload = function() {
+    // ... tumhara code ...
+};
